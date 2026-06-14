d5df63454f39e01cd63a3f984aba2194c268587a
Marcio Ferreira Lima <88352524+MarcioMicro@users.noreply.github.com>
2026-06-13 22:00:55 -0300

    Bolão Copa 2026 - versão inicial

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbz7FAIwtjfYtqZATUTOLNVe7ZPlf3e0jJboY1QJzjtygJxKSxdbptvLG-e4G-wuviJz/exec',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyMneOQwAwXIIS1LIJXHTLlH128_Y9XvCJCcjEgvVs3vo8eXvabucWhW-N9vCfGHVpN/exec',
   PONTOS_PLACAR_EXATO: 8,
   PONTOS_SALDO_CERTO: 5,
   PONTOS_VENCEDOR_CERTO: 3,
